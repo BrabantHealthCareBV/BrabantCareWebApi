6c4fd8a0799c417385483141e29aed52d10cdbac
S.k2004 <79660690+s-kroonen@users.noreply.github.com>
2025-03-30 03:13:18 +0200

    changed treatmentmoment structure

--- Pages/TreatmentPlanCareMoments/Index.cshtml.cs
+++ Pages/TreatmentPlanCareMoments/Index.cshtml.cs
@@ -1,27 +1,21 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using BrabantCareWebApi.Models;
-using BrabantCareWebApi.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using BrabantCareWebApi.Repositories;
+using BrabantCareWebApi.Models;
 
-namespace BrabantCareWebApi.Pages.TreatmentPlanCareMoments
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly TreatmentPlanRepository _treatmentPlanRepo;
+
+    public IndexModel(TreatmentPlanRepository treatmentPlanRepo)
     {
-        private readonly TreatmentPlanCareMomentRepository _repository;
+        _treatmentPlanRepo = treatmentPlanRepo;
+    }
 
-        public IndexModel(TreatmentPlanCareMomentRepository repository)
-        {
-            _repository = repository;
-        }
+    public IEnumerable<TreatmentPlan> TreatmentPlans { get; set; } = new List<TreatmentPlan>();
 
-        public IEnumerable<TreatmentPlanCareMoment> CareMoments { get; set; } = new List<TreatmentPlanCareMoment>();
-
-        [BindProperty]
-        public Guid TreatmentPlanID { get; set; }
-
-        public async Task OnGetAsync()
-        {
-            CareMoments = await _repository.ReadAsync(TreatmentPlanID);
-        }
+    public async Task OnGetAsync()
+    {
+        TreatmentPlans = await _treatmentPlanRepo.ReadAsync();
     }
 }
