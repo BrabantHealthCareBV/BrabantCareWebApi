6ad1ab04cd94b134994a7dafd1b59123064d1bd1
S.k2004 <79660690+s-kroonen@users.noreply.github.com>
2025-03-31 16:12:16 +0200

    patient allow nulls

--- Models/Patient.cs
+++ Models/Patient.cs
@@ -5,8 +5,8 @@ namespace BrabantCareWebApi.Models
         public Guid ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public DateTime Birthdate { get; set; } 
-        public DateTime NextAppointmentDate { get; set; }
+        public DateTime? Birthdate { get; set; } 
+        public DateTime? NextAppointmentDate { get; set; }
         public Guid GuardianID { get; set; }
         public Guid TreatmentPlanID { get; set; }
         public Guid? DoctorID { get; set; }
