20e4d43477fdfc723fd3f4446bfbb93551f34a87
S.k2004 <79660690+s-kroonen@users.noreply.github.com>
2025-04-02 16:11:59 +0200

    user id for patient isnt required

--- Models/Doctor.cs
+++ Models/Doctor.cs
@@ -5,7 +5,7 @@ namespace BrabantCareWebApi.Models
     {
         public Guid ID { get; set; }
         public string Name { get; set; }
-        public string Specialization { get; set; }
+        public string? Specialization { get; set; }
         //public List<Guid> PatientIDs { get; set; } // optional, ensure this is handled if necessary
     }
 
