3b7dadb1cbca61fa138b503628f662785f9381bc
S.k2004 <79660690+s-kroonen@users.noreply.github.com>
2025-04-02 11:06:29 +0200

    trying to fix manage page

--- Pages/TreatmentPlanCareMoments/Manage.cshtml.cs
+++ Pages/TreatmentPlanCareMoments/Manage.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc;
 using BrabantCareWebApi.Repositories;
 using BrabantCareWebApi.Models;
+using System.Linq;
 
 public class ManageModel : PageModel
 {
@@ -17,38 +18,56 @@ public class ManageModel : PageModel
     [BindProperty(SupportsGet = true)]
     public Guid TreatmentPlanID { get; set; }
 
+    public List<TreatmentPlanCareMoment> LinkedCareMoments { get; set; } = new();
     public IEnumerable<CareMoment> AvailableCareMoments { get; set; } = new List<CareMoment>();
-    public IEnumerable<TreatmentPlanCareMoment> LinkedCareMoments { get; set; } = new List<TreatmentPlanCareMoment>();
 
     public async Task OnGetAsync()
     {
-        LinkedCareMoments = await _tpCareMomentRepo.ReadAsync(TreatmentPlanID);
-        AvailableCareMoments = await _careMomentRepo.ReadAsync();
+        var linked = await _tpCareMomentRepo.ReadAsync(TreatmentPlanID);
+        var allCareMoments = await _careMomentRepo.ReadAsync();
+
+        LinkedCareMoments = linked.Select(l => new TreatmentPlanCareMoment
+        {
+            CareMomentID = l.CareMomentID,
+            TreatmentPlanID = l.TreatmentPlanID,
+            Order = l.Order,
+            CareMomentName = allCareMoments.FirstOrDefault(c => c.ID == l.CareMomentID)?.Name ?? "Unknown"
+        }).ToList();
+
+        AvailableCareMoments = allCareMoments;
     }
 
-    public async Task<IActionResult> OnPostAddAsync(Guid careMomentId, Guid TreatmentPlanID)
+    public async Task<IActionResult> OnPostAddAsync(Guid TreatmentPlanID, Guid careMomentId)
     {
-        if (TreatmentPlanID == Guid.Empty || careMomentId == Guid.Empty)
-        {
-            ModelState.AddModelError(string.Empty, "Invalid data.");
-            return Page();
-        }
-
         var newLink = new TreatmentPlanCareMoment
         {
             TreatmentPlanID = TreatmentPlanID,
             CareMomentID = careMomentId,
-            Order = (await _tpCareMomentRepo.ReadAsync(TreatmentPlanID)).Count() + 1
+            Order = LinkedCareMoments.Count + 1
         };
 
         await _tpCareMomentRepo.InsertAsync(newLink);
         return RedirectToPage(new { TreatmentPlanID });
     }
 
-
-    public async Task<IActionResult> OnPostRemoveAsync(Guid careMomentId)
+    public async Task<IActionResult> OnPostRemoveAsync(Guid TreatmentPlanID, Guid careMomentId)
     {
         await _tpCareMomentRepo.DeleteAsync(TreatmentPlanID, careMomentId);
         return RedirectToPage(new { TreatmentPlanID });
     }
+
+    public async Task<IActionResult> OnPostUpdateOrderAsync(Guid TreatmentPlanID, Guid careMomentId, int newOrder)
+    {
+        var updatedEntity = new TreatmentPlanCareMoment
+        {
+            TreatmentPlanID = TreatmentPlanID,
+            CareMomentID = careMomentId,
+            Order = newOrder
+        };
+
+        await _tpCareMomentRepo.UpdateAsync(updatedEntity);
+        return RedirectToPage(new { TreatmentPlanID });
+    }
+
 }
+
